e3c682c737ff69be0ba638ed8c28dd09d4f9a52e
kamil <kamil_grabowski@vp.pl>
2018-07-03 19:35:05 +0200

    First

---
+++ src/components/containers/Users.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+class User extends Component {
+
+    constructor() {
+        super();
+        this.state ={
+            userName:'',
+            password:'',
+        }
+    }
+
+    onUserUpdate(event){
+        console.log("Siemak");
+        console.log('onUserUpdate: ' + event.target.value);
+    }
+
+    render(){
+        console.log("Elo");
+
+        return (
+            <div style={{paddingLeft:24}}>
+                There is User container!!<br/>
+                <input onChange={this.onUserUpdate.bind(this, 'userName')}
+                       type="text"
+                       placeholder="userName"
+
+                        />
+                <br/>
+                <br/>
+                <input type="password"
+                       placeholder="password"
+                       onChange={this.onUserUpdate.bind(this, 'password')}
+                /><br/>
+                <button>Add User</button>
+            </div>
+        )
+    }
+}
+
+export default User;
